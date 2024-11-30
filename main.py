adae9147198131e0b72a78d13d5f5b0a3d654136
timosarkar <timosarkartimo@gmail.com>
2024-11-30 18:59:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ from qiskit import *
 from qiskit_aer import Aer
 from qiskit.compiler import transpile
 
-qasm_str = """OPENQASM 2.0;
-include "qelib1.inc";
-qreg q[2];
-creg c[2];
-h q[0];
-cx q[0],q[1];
-measure q -> c;
-"""
+# qasm_str = """OPENQASM 2.0;
+# include "qelib1.inc";
+# qreg q[2];
+# creg c[2];
+# h q[0];
+# cx q[0],q[1];
+# measure q -> c;
+# """
 
-# From str.
-#qc = QuantumCircuit.from_qasm_str(qasm_str)
+# qc = QuantumCircuit.from_qasm_str(qasm_str)
 qc = QuantumCircuit.from_qasm_file("file.qasm")
 
 backend = Aer.get_backend("qasm_simulator")
@@ -22,4 +21,3 @@ job = backend.run(new)
 result = job.result()
 counts = result.get_counts()
 print(counts)
-
