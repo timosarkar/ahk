273228c5dac32651eac4bca6aaea5beb90c3b614
timosarkar <timosarkartimo@gmail.com>
2024-12-01 10:36:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,31 @@ from qiskit import *
 from qiskit_aer import Aer
 from qiskit.compiler import transpile
 
+import json
+
+# load all mapped possible program outputs into memory
+outputs = json.loads("outputs.json")
+
+# load QASM code from file
 qc = QuantumCircuit.from_qasm_file("file.qasm")
 
-backend = Aer.get_backend("qasm_simulator")
-new = transpile(qc, backend)
-job = backend.run(new)
+# program loop here--------
 
-result = job.result()
-counts = result.get_counts()
-print(counts)
+#while True:
+
+# program starts with the default state
+state = "0000000000000010011011"
+print(outputs[state]) # print the value mapped to current program state to stdout
+
+
+# transpile & run the program from here---------
+
+# initiate new Aer simulator
+#backend = Aer.get_backend("qasm_simulator")
+
+#new = transpile(qc, backend)
+
+#job = backend.run(new)
+#result = job.result()
+#counts = result.get_counts()
+#print(counts)
